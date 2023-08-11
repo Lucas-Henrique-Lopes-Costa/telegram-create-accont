7020cb441e23b1bdc64ce78847e782c95d9372a2
Lucas Henrique Lopes Costa <lucass20586@gmail.com>
2023-08-10 23:05:44 -0300

    teste

---
+++ teste.py
@@ -0,0 +1,17 @@
+import requests
+
+# set the proxy url
+proxy_url = 'geo.iproyal.com'
+port = '32325'
+user = 'megawinproxy'
+password = 'arvore11_country-br_city-saopaulo'
+
+# Realiza a requisição HTTP utilizando o proxy
+response = requests.get('http://httpbin.org/ip', proxies=dict(http=f'socks5://{user}:{password}@{proxy_url}:{port}', https=f'socks5://{user}:{password}@{proxy_url}:{port}'))
+
+# Verifica o resultado
+if response.status_code == 200:
+    print("Requisição bem-sucedida!")
+    print(response.text)
+else:
+    print(f"Erro na requisição. Código de status: {response.status_code}")
